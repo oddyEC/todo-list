5de5da3dbe176db6b2934ffcd15b3cc44f505657
Diego Marquez <dimarque@pichincha.com>
2024-01-12 17:56:05 -0500

    change directive

--- todo-list/src/app/strike.directive.ts
+++ todo-list/src/app/strike.directive.ts
@@ -1,23 +1,30 @@
-import { Directive, ElementRef, HostListener, Input, OnInit } from '@angular/core';
+import {
+  Directive,
+  ElementRef,
+  HostListener,
+  Input,
+  OnInit,
+} from '@angular/core';
 
 @Directive({
-  selector: '[appStrike]'
+  selector: '[appStrike]',
 })
 export class StrikeDirective implements OnInit {
   @Input() appStrike: boolean = false;
 
-  constructor(private elementRef: ElementRef) { }
+  constructor(private elementRef: ElementRef) {}
   ngOnInit(): void {
-    this.elementRef.nativeElement.style.textDecoration = 
-    this.appStrike ? 'line-through' : 'none';
+    this.elementRef.nativeElement.style.textDecoration = this.appStrike
+      ? 'line-through'
+      : 'none';
   }
 
   @HostListener('mouseenter')
-  addLine(){
+  addLine() {
     this.elementRef.nativeElement.style.textDecoration = 'line-through';
   }
   @HostListener('mouseleave')
-  removeLine(){
+  removeLine() {
     this.elementRef.nativeElement.style.textDecoration = 'none';
-      }
+  }
 }
